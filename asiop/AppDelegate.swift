79edb8cd9c9f1b59223c25ec9600256a604c0769
Lubomír Klimeš <lubomir.klimes@hotmail.com>
2016-07-22 00:50:03 +0200

    Added statusMenu with icon and quit command

--- asiop/AppDelegate.swift
+++ asiop/AppDelegate.swift
@@ -12,10 +12,22 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var statusMenu: NSMenu!
 
-
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+    
+    @IBAction func quitClicked(sender: NSMenuItem) {
+        NSApplication.sharedApplication().terminate(self)
+    }
+    
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        let icon = NSImage(named: "statusIcon")
+        icon?.template = true // best for dark mode
+        statusItem.image = icon
+        //statusItem.title = "ATARI SIO"
+        statusItem.menu = statusMenu
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
