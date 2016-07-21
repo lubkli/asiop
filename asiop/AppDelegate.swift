ada73861a1eb1de8fd087b186a6290c280ead654
Lubomír Klimeš <lubomir.klimes@hotmail.com>
2016-07-22 00:10:03 +0200

    Initial Commit

---
+++ asiop/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  asiop
+//
+//  Created by Lubomír Klimeš on 22/07/16.
+//  Copyright © 2016 lubkli. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
